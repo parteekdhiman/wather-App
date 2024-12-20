3011c27d038f5d4b99a9ec2bd5fc078ab06482fb
Parteek758 <Pkdhiman414@gmail.com>
2024-12-20 14:01:33 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -1,36 +1,42 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const hbs = require("hbs")
-const env = require("dotenv")
-env.config()
+const hbs = require("hbs");
+const env = require("dotenv");
+env.config();
+
 const port = process.env.PORT || 8000;
 
-// public static path 
-const static_path = (path.join(__dirname, "../public"));
-const templates_path = (path.join(__dirname, "../templates/views"));
-const partilas_path = (path.join(__dirname, "../templates/partilas"));
+// Static and templates paths
+const static_path = path.join(__dirname, "../public");
+const templates_path = path.join(__dirname, "../templates/views");
+const partials_path = path.join(__dirname, "../templates/partials");
+
+// Set up view engine and views path
 app.set('view engine', 'hbs');
 app.set('views', templates_path);
-hbs.registerPartials(partilas_path)
 
+// Register partials
+hbs.registerPartials(partials_path);
 
-app.use(express.static(static_path))
+// Serve static files
+app.use(express.static(static_path));
 
+// Routing
+app.get("/", (req, res) => {
+    res.render("index");
+});
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+app.get("/weather", (req, res) => {
+    res.render("weather"); // Ensure weather.hbs exists in the views folder
+});
+app.get("*", (req, res) => {
+    res.render("404error");
+});
 
-// routing 
-app.get("/",(req,res)=>{
-    res.render("index")
-})
-app.get("/about",(req,res)=>{
-    res.render("about")
-})
-app.get("/weather",(req,res)=>{
-    res.render("weather")
-})
-app.get("*",(req,res)=>{
-    res.render("404error")
-})
-app.listen(port,()=>{
-console.log(`the port ${port} is listening`)
-})
+// Start server
+app.listen(port, () => {
+    console.log(`The server is running on port ${port}`);
+});
